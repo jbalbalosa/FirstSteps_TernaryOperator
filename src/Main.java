d3b83e7b7ef2c5cc92f2e37e97f99e210970a7bf
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-17 16:40:21 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Ternary Operator"); //iIf condition in Visual Basic
+
+        boolean isCar = false;
+        if(isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
+        //isCar = true;
+        boolean wasCar = isCar ? true : false; //This is iiF in Visual Basic
+        if (wasCar){
+            System.out.println("wasCar is " + wasCar);
+        }
+
     }
 }
